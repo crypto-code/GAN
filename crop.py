9c64ddb8585244a8add4b8847458e631fd07fa66
Atin Sakkeer <atinsaki@gmail.com>
2019-11-12 19:56:33 +0400

    Add files via upload

---
+++ crop.py
@@ -0,0 +1,23 @@
+from PIL import Image
+import argparse
+import os
+
+def main(src, dst):
+  im = Image.open(src)
+  k=0
+  if not os.path.exists(dst):
+        os.mkdir(dst)
+  for i in range(0,1024,128):
+    for j in range(0,1024,128):
+      imnew = im.crop((i,j,i+128,j+128))
+      imnew =imnew.resize((512,512), Image.BICUBIC)
+      imnew.save((r"%s\[%d].jpg" % (dst,k)), quality=100)
+      k=k+1
+
+
+if __name__=="__main__":
+  parser = argparse.ArgumentParser(description='Resize Input Images')
+  parser.add_argument('--input',type=str,required=True,help='Input Image')
+  parser.add_argument('--output',type=str,required=True,help='Output Directory')
+  args = parser.parse_args()
+  main(args.input, args.output)
